3c86e52ad44b84415a47550346563d86c306e268
João Vítor <joaovitorsf7@hotmail.com>
2020-12-22 12:17:06 -0300

    "Veja o projeto".

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -33,6 +33,8 @@ firebase.auth().onAuthStateChanged((firebaseUser) => {
     .get()
     .then(function (querySnapshot) {
       const docSnapshots = querySnapshot.docs;
+      let x = 0;
+      let y = 0;
 
       for (let i = 0; i < querySnapshot.size; i++) {
         let card = document.createElement("div");
@@ -42,18 +44,35 @@ firebase.auth().onAuthStateChanged((firebaseUser) => {
         cardContent.classList = "card-content";
 
         let cardTitle = document.createElement("span");
-        cardTitle.classList = "card-title";
+        cardTitle.classList = `card-title i${x}`;
         cardTitle.textContent = docSnapshots[i].data().nome;
         cardContent.appendChild(cardTitle);
 
         let descProjeto = document.createElement("p");
+        descProjeto.classList = `card-desc j${y}`;
         descProjeto.textContent = docSnapshots[i].data().descricao;
         cardContent.appendChild(descProjeto);
 
         let cardAction = document.createElement("div");
         cardAction.classList = "card-action";
         aCardAction = document.createElement("a");
-        aCardAction.setAttribute("href", "#");
+        aCardAction.setAttribute(
+          "onClick",
+          `container.style.display = "flex";
+           container.style.flexDirection = "column";
+           container.innerHTML = '<header><h1>' + document.querySelector('.card-title.i${x}').textContent + '</h1></header><br />' + '<p style="text-align: center;">' + document.querySelector('.card-desc.j${y}').textContent + '</p>';
+           container.innerHTML += '<div class="fixed-action-btn"> <a class="btn-floating btn-large teal" style="font-size: 25px"> + </a> <ul><li><a class="btn-floating teal" style="font-size: 25px;display: flex;justify-content: center;align-items: center;" title="Adicionar aluno" href="./project/addAluno.html">➕</a></li></ul></div>'
+           let elems = document.querySelectorAll(".fixed-action-btn");
+           const options = new Object({
+             direction: "left",
+             hoverEnabled: false,
+             toolbarEnabled: false,
+           });
+           M.FloatingActionButton.init(elems, options);
+           `
+        );
+        x++;
+        y++;
         aCardAction.classList = "btn btn-primary";
         aCardAction.textContent = "Veja o projeto";
         cardAction.appendChild(aCardAction);
